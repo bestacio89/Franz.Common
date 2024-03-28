01d60aa4e9f7dd4d151bf13ddb8dd706e23b8b69
Bernardo Estacio <bernardo.estacio89@gmail.com>
2024-03-28 16:55:29 +0000

    Merged PR 90: [Task] #385 Azure Cosmos DB Service Extensions
    Related work items: #364

---
+++ sources/Franz.Common.CosmosDB/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MinervaFramework.Common.CosmosDB;
+public static class ServiceRegistration
+{
+  public static void ConfigureCosmosDb(IServiceCollection services, string cosmosDbEndpoint, string cosmosDbKey, string cosmosDbDatabaseName)
+  {
+    var cosmosDbConfig = new CosmosDbConfig
+    {
+      Endpoint = cosmosDbEndpoint,
+      Key = cosmosDbKey,
+      DatabaseName = cosmosDbDatabaseName
+    };
+
+    services.AddDbContext<CosmosDbContext>(options =>
+        options.UseCosmos(
+            cosmosDbConfig.Endpoint,
+            cosmosDbConfig.Key,
+            cosmosDbConfig.DatabaseName));
+
+    // Other services and dependencies registration
+  }
+
+  // Other service registration configurations
+}
