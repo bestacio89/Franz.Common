3bd61f59fb24893805ac062550106d88f6ac3f75
Bernardo Estacio <bernardo.estacio89@gmail.com>
2024-03-28 13:11:14 +0000

    Merged PR 73: [Task] #395 Base Automapper Development

---
+++ sources/Franz.Common.AutoMapper/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace Microsoft.Extensions.DependencyInjection;
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+  {
+    services
+      .AddInheritedClassSingleton<Profile>()
+      .AddNoDuplicateSingleton(serviceProvider =>
+      {
+        var profiles = serviceProvider.GetServices<Profile>();
+
+        var mapperConfiguration = new MapperConfiguration(configuration =>
+        {
+          configuration.AddProfiles(profiles);
+        });
+        var result = mapperConfiguration.CreateMapper();
+
+        return result;
+      });
+
+    return services;
+  }
+}
