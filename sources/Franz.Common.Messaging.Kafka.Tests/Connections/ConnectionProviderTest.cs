2f17325b34039afdad191347264ccd2947a44476
Bernardo Estacio Abreu <Bernardo.estacio89@gmail.com>
2024-04-09 22:35:46 +0200

    [Fix]  Assertion

--- sources/Franz.Common.Messaging.Kafka.Tests/Connections/ConnectionProviderTest.cs
+++ sources/Franz.Common.Messaging.Kafka.Tests/Connections/ConnectionProviderTest.cs
@@ -66,7 +66,7 @@ namespace Franz.Common.Messaging.Kafka.Tests.Connections
       var connectionProvider = new ConnectionProvider(connectionFactoryProviderMock.Object);
 
       // Act and Assert
-      !Assert.Throws<Exception>(() => connectionProvider.Dispose());
+      Assert.IsNotType<Exception>(() => connectionProvider.Dispose());
     }
   }
 }
