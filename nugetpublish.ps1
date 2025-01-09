f928549e097767efb3c4d8728f268ba87d2e23c7
Bernardo Estacio Abreu <bernardo.estacioabreu@exakis-nelite.com>
2025-01-09 10:22:39 +0100

    [Task] Nuget publisher script functional and corrected.

--- nugetpublish.ps1
+++ nugetpublish.ps1
@@ -1,9 +1,9 @@
 # Specify the NuGet repository URL and API key
-$FEED_URL = "https://api.nuget.org/v3/index.json" # Replace with your NuGet feed URL if different
-$API_KEY = "oy2c4zz565tuclyxijrakv3xlbzcs2quvkyyhlbkqq7ycm" # Your NuGet API key
+$FEED_URL = "https://api.nuget.org/v3/index.json" # NuGet.org feed URL
+$API_KEY = "oy2c4zz565tuclyxijrakv3xlbzcs2quvkyyhlbkqq7ycm" # Your NuGet.org API key
 
 # Directory where NuGet packages are located
-$PACKAGES_DIRECTORY = "AzureArtifacts" # Replace with the path to your NuGet packages directory
+$PACKAGES_DIRECTORY = "NuGetPackages" # Replace with your actual NuGet package directory
 
 # Ensure the directory exists
 if (-Not (Test-Path -Path $PACKAGES_DIRECTORY)) {
@@ -12,17 +12,21 @@ if (-Not (Test-Path -Path $PACKAGES_DIRECTORY)) {
     exit 1
 }
 
+# Perform NuGet packaging
+Write-Host "Packaging projects into NuGet packages..."
+dotnet pack -c Release -o $PACKAGES_DIRECTORY
+
 # Retrieve a list of all existing packages from the feed
-Write-Host "Retrieving existing packages from the feed..."
+Write-Host "Retrieving existing packages from NuGet feed..."
 $existingPackages = & dotnet nuget list source --source $FEED_URL --api-key $API_KEY
 if (!$existingPackages) {
-    Write-Host "Failed to retrieve packages from the feed." -ForegroundColor Red
+    Write-Host "Failed to retrieve packages from the NuGet feed." -ForegroundColor Red
     Read-Host "Press Enter to exit"
     exit 1
 }
 
 # Publish each package in the directory
-Write-Host "Publishing packages..."
+Write-Host "Publishing packages to NuGet feed..."
 $packages = Get-ChildItem -Path $PACKAGES_DIRECTORY -Filter *.nupkg
 foreach ($package in $packages) {
     # Extract package metadata
@@ -32,6 +36,7 @@ foreach ($package in $packages) {
 
     # Check if a package with the same name and version already exists
     $duplicatePackage = $existingPackages | Where-Object { $_.Id -eq $packageName -and $_.Version -eq $packageVersion }
+
     if ($null -eq $duplicatePackage) {
         Write-Host "Publishing $($package.Name)..."
         dotnet nuget push $package.FullName --source $FEED_URL --api-key $API_KEY --skip-duplicate
