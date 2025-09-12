74e8f921d25ffa727d302d4ca688628f997984a3
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-09-12 21:22:41 +0200

    feat(multitenancy): Implement default domain and tenant resolution pipelines

---
+++ sources/Franz.Common.Http.MultiTenancy/DefaultDomainResolutionPipeline.cs
@@ -0,0 +1,25 @@
+﻿using Franz.Common.MultiTenancy;
+
+namespace Franz.Common.Messaging.MultiTenancy.Pipeline;
+
+public class DefaultDomainResolutionPipeline : IDomainResolutionPipeline
+{
+  private readonly IEnumerable<IDomainResolver> _resolvers;
+
+  public DefaultDomainResolutionPipeline(IEnumerable<IDomainResolver> resolvers)
+  {
+    _resolvers = resolvers.OrderBy(r => r.Order);
+  }
+
+  public async Task<DomainResolutionResult> ResolveAsync(object? context = null)
+  {
+    foreach (var resolver in _resolvers)
+    {
+      var result = await resolver.ResolveDomainAsync(context);
+      if (result.Success)
+        return result;
+    }
+
+    return DomainResolutionResult.FailedResult("No resolver could determine domain.");
+  }
+}
---
+++ sources/Franz.Common.Http.MultiTenancy/DefaultTenantResolutionPipeline.cs
@@ -0,0 +1,26 @@
+﻿using Franz.Common.MultiTenancy;
+using Franz.Common.MultiTenancy.TenantResolution;
+
+namespace Franz.Common.Messaging.MultiTenancy.Pipeline;
+
+public class DefaultTenantResolutionPipeline : ITenantResolutionPipeline
+{
+  private readonly IEnumerable<ITenantResolver> _resolvers;
+
+  public DefaultTenantResolutionPipeline(IEnumerable<ITenantResolver> resolvers)
+  {
+    _resolvers = resolvers.OrderBy(r => r.Order);
+  }
+
+  public async Task<TenantResolutionResult?> ResolveAsync(object? context = null)
+  {
+    foreach (var resolver in _resolvers)
+    {
+      var result = await resolver.ResolveTenantAsync(context);
+      if (result?.Tenant != null)
+        return result;
+    }
+
+    return null;
+  }
+}
