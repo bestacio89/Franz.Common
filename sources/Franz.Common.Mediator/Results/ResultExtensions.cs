59228f8665085268328a48aef1af88a7122dd4a2
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-09-15 22:53:42 +0200

    [Feat] Result generics

---
+++ sources/Franz.Common.Mediator/Results/ResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Franz.Common.Mediator.Errors;
+
+namespace Franz.Common.Mediator.Results
+{
+  public static class ResultExtensions
+  {
+    public static Result<T> ToResult<T>(this T value) =>
+        Result<T>.Success(value);
+
+    public static Result<T> ToFailure<T>(this string message) =>
+        Result<T>.Failure(new Error("Error", message, null));
+
+    public static Result<T> ToFailure<T>(this Error error) =>
+        Result<T>.Failure(error);
+  }
+}
