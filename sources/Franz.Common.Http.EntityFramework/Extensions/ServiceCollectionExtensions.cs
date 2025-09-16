8d47b97aa5ce02e1704a320e450f475066b6b33a
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-09-16 19:32:33 +0200

    [Patch] Service collection failures

--- sources/Franz.Common.Http.EntityFramework/Extensions/ServiceCollectionExtensions.cs
+++ sources/Franz.Common.Http.EntityFramework/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 using Franz.Common.EntityFramework;
+using Franz.Common.EntityFramework.Oracle.Extensions;
+using Franz.Common.EntityFramework.Postgres.Extensions;
+using Franz.Common.EntityFramework.SQLServer.Extensions;
 using Franz.Common.Http.EntityFramework.Transactions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -6,17 +9,38 @@ using Microsoft.Extensions.Hosting;
 namespace Microsoft.Extensions.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
-  public static IServiceCollection AddDatabase<TDbContext>(this IServiceCollection services, IHostEnvironment hostEnvironment, IConfiguration configuration)
+
+  public static IServiceCollection AddDatabase<TDbContext>(
+    this IServiceCollection services,
+    IHostEnvironment hostEnvironment,
+    IConfiguration configuration)
     where TDbContext : DbContextBase
   {
     services
-      .AddDatabaseTransactionPerHttpCall()
-      .AddMariaDatabase<TDbContext>(configuration)
-      .AddGenericRepositories<TDbContext>()
-      .AddBehaviors();
+        .AddDatabaseTransactionPerHttpCall();
+
+    // Pick database provider
+    var provider = configuration["Database:Provider"]?.ToLowerInvariant();
+
+    services = provider switch
+    {
+      "mariadb" => services.AddMariaDatabase<TDbContext>(configuration),
+      "oracle" => services.AddOracleDatabase<TDbContext>(configuration),
+      "postgres" => services.AddPostgresDatabase<TDbContext>(configuration),
+      "sqlserver" => services.AddSqlServerDatabase<TDbContext>(configuration),
+      _ => throw new InvalidOperationException(
+                        $"Unsupported DB provider '{provider}'. " +
+                        "Use one of: MariaDb, Oracle, Postgres, SqlServer.")
+    };
+
+    services
+        .AddDatabaseTransactionPerHttpCall()
+        .AddGenericRepositories<TDbContext>()
+        .AddBehaviors();
 
     return services;
   }
+
 
   public static IServiceCollection AddDatabaseTransactionPerHttpCall(this IServiceCollection services)
   {
