35712a0f2741e3b7f41534dbb45b28e1292187a1
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-12-21 20:55:37 +0100

    [DI] Wiring Infra for Tests

--- tests/Franz.Common.Messaging.Hosting.RabbitMQ.Tests/Hosting/RabbitMQMessageModelTests.cs
+++ tests/Franz.Common.Messaging.Hosting.RabbitMQ.Tests/Hosting/RabbitMQMessageModelTests.cs
@@ -1,7 +1,11 @@
-﻿using Franz.Common.Messaging.Configuration;
+﻿using Franz.Common.Mediator.Extensions;
+using Franz.Common.Messaging.Configuration;
+using Franz.Common.Messaging.Hosting.RabbitMQ.Tests.Fakes;
 using Franz.Common.Messaging.Hosting.RabbitMQ.Tests.Fixtures;
 using Franz.Common.Messaging.RabbitMQ.Connections;
+using Franz.Common.Messaging.RabbitMQ.Extensions;
 using Franz.Common.Messaging.RabbitMQ.Modeling;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client;
 using System;
@@ -25,28 +29,22 @@ public class RabbitMqMessageModelTests
   {
     var services = new ServiceCollection();
 
-    services.Configure<MessagingOptions>(opts =>
-    {
-      opts.HostName = _fixture.Host;
-      opts.Port = _fixture.Port;
-    });
-
-    services.AddSingleton<IConnectionFactoryProvider, ConnectionFactoryProvider>();
-    services.AddSingleton<IConnectionProvider, ConnectionProvider>();
-    services.AddScoped<IRabbitMqMessageModel, RabbitMqMessageModel>();
+    var config = new ConfigurationBuilder()
+      .AddInMemoryCollection(new Dictionary<string, string?>
+      {
+        ["Messaging:HostName"] = _fixture.Host,
+        ["Messaging:Port"] = _fixture.Port.ToString()
+      })
+      .Build();
+    services.AddFranzMediator(new[]{
+          typeof(TestIntegrationEvent).Assembly});
+    services.AddRabbitMQMessaging(config);
 
     using var provider = services.BuildServiceProvider();
     using var scope = provider.CreateScope();
 
-    var model = scope.ServiceProvider.GetRequiredService<IRabbitMqMessageModel>();
-    var connection = scope.ServiceProvider.GetRequiredService<IConnectionProvider>().Current;
-
-    using var channel = await connection.CreateChannelAsync();
-    await channel.ExchangeDeclareAsync(
-      exchange: "franz.tests.exchange",
-      type: ExchangeType.Fanout,
-      durable: false,
-      autoDelete: true);
+    var model = scope.ServiceProvider
+      .GetRequiredService<IRabbitMqMessageModel>();
 
     await model.ProduceAsync(
       exchange: "franz.tests.exchange",
