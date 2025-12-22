61279239493d31711349d785f2ec0f55292adf31
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-12-22 20:09:30 +0100

    [Fix][Tests] Fixing Pipeline Building

--- tests/Franz.Common.Caching.Testing/Extensions/AddFranzMediatorCaching.cs
+++ tests/Franz.Common.Caching.Testing/Extensions/AddFranzMediatorCaching.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-namespace Franz.Common.Caching.Testing.Extensions;
-
-using FluentAssertions;
+﻿using FluentAssertions;
 using Franz.Common.Caching.Extensions;
 using Franz.Common.Caching.Pipelines;
 using Franz.Common.Caching.Testing.Models;
-using Franz.Common.Mediator.Extensions;
 using Franz.Common.Mediator.Pipelines.Core;
 using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Franz.Common.Caching.Testing.Extensions;
 
 public sealed class AddFranzMediatorCachingTests
 {
@@ -22,16 +16,19 @@ public sealed class AddFranzMediatorCachingTests
   [Fact]
   public void Should_Register_CachingPipeline()
   {
-    using var sp = ServiceTestHelper.Build(services =>
+    using var serviceProvider = ServiceTestHelper.Build(services =>
     {
       services.AddLogging();
-      services.AddFranzMemoryCaching();
-      services.AddFranzMediatorCaching();
+      services.AddFranzMemoryCaching();     // provides ICacheProvider & friends
+      services.AddFranzMediatorCaching();   // registers caching pipeline
     });
 
-    var pipelines = sp.GetServices<IPipeline<TestRequest, TestResponse>>();
+    var pipelines = serviceProvider
+      .GetServices<IPipeline<TestRequest, TestResponse>>()
+      .ToList();
 
-    pipelines.Should().ContainSingle(p =>
-      p is CachingPipeline<TestRequest, TestResponse>);
+    pipelines.Should().ContainSingle();
+    pipelines.Single().Should()
+      .BeOfType<CachingPipeline<TestRequest, TestResponse>>();
   }
 }
