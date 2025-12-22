14ab3fae70292a68dc4d52d6bf82dc3556c93017
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-12-22 19:16:26 +0100

    [Fix][Tests] Add logging references to the middleware pipeline

--- tests/Franz.Common.Caching.Testing/Extensions/AddFranzMediatorCaching.cs
+++ tests/Franz.Common.Caching.Testing/Extensions/AddFranzMediatorCaching.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 namespace Franz.Common.Caching.Testing.Extensions;
 
 using FluentAssertions;
 using Franz.Common.Caching.Extensions;
 using Franz.Common.Caching.Pipelines;
 using Franz.Common.Caching.Testing.Models;
+using Franz.Common.Mediator.Extensions;
 using Franz.Common.Mediator.Pipelines.Core;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,6 +23,7 @@ public sealed class AddFranzMediatorCachingTests
     {
       services.AddFranzMemoryCaching();
       services.AddFranzMediatorCaching();
+      services.AddLogging();
     });
 
     var pipeline = sp.GetServices(typeof(IPipeline<,>));
