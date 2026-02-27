15842f344ddb0f70b5aa73449d6d0f9b23fea2c3
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-02-27 16:43:13 +0100

    [Refactor]: Update IMediatorObserver to use Guid for correlation IDs
    - Robustness improved with guid

--- sources/Franz.Common.Mediator/Diagnostics/IMediatorObserver.cs
+++ sources/Franz.Common.Mediator/Diagnostics/IMediatorObserver.cs
@@ -2,13 +2,13 @@
 {
   public interface IMediatorObserver
   {
-    Task OnRequestStarted(object request, string correlationId, CancellationToken cancellationToken);
-    Task OnRequestCompleted(object request, object? response, string correlationId, TimeSpan duration, CancellationToken cancellationToken);
-    Task OnRequestFailed(object request, Exception exception, string correlationId, TimeSpan duration, CancellationToken cancellationToken);
+    Task OnRequestStarted(object request, Guid correlationId, CancellationToken cancellationToken);
+    Task OnRequestCompleted(object request, object? response, Guid correlationId, TimeSpan duration, CancellationToken cancellationToken);
+    Task OnRequestFailed(object request, Exception exception, Guid correlationId, TimeSpan duration, CancellationToken cancellationToken);
 
     // New for notification handlers
-    Task OnNotificationHandlerStarted(object notification, Type handlerType, string correlationId, CancellationToken cancellationToken);
-    Task OnNotificationHandlerCompleted(object notification, Type handlerType, string correlationId, TimeSpan duration, CancellationToken cancellationToken);
-    Task OnNotificationHandlerFailed(object notification, Type handlerType, string correlationId, Exception exception, TimeSpan duration, CancellationToken cancellationToken);
+    Task OnNotificationHandlerStarted(object notification, Type handlerType, Guid correlationId, CancellationToken cancellationToken);
+    Task OnNotificationHandlerCompleted(object notification, Type handlerType, Guid correlationId, TimeSpan duration, CancellationToken cancellationToken);
+    Task OnNotificationHandlerFailed(object notification, Type handlerType, Guid correlationId, Exception exception, TimeSpan duration, CancellationToken cancellationToken);
   }
 }
