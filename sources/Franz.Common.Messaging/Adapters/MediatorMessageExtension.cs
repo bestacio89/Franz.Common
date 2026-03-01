fbb7629528097fab140c9b6332bb5c71ef5340e9
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-03-01 19:36:11 +0100

    [Fix][Messaging] Fix Mediator Adapter
    - Fix Serialization and deserialization of messages

--- sources/Franz.Common.Messaging/Adapters/MediatorMessageExtension.cs
+++ sources/Franz.Common.Messaging/Adapters/MediatorMessageExtension.cs
@@ -1,27 +1,32 @@
 ﻿using Franz.Common.Mediator.Messages;
 using Franz.Common.Messaging.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Franz.Common.Messaging.Adapters;
+
 public static class MediatorMessageExtensions
 {
+  // Hardened options for 1.7.8 to ensure records and init-only props serialize correctly
+  private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web);
+
   public static Message ToMessage(this ICommand command)
   {
-    var msg = new Message(JsonSerializer.Serialize(command));
+    // Use the options here!
+    var json = JsonSerializer.Serialize((object)command, _options);
+    var msg = new Message(json);
+
     msg.MessageType = command.GetType().FullName;
-    msg.CorrelationId = Guid.CreateVersion7();
+    msg.CorrelationId = Guid.CreateVersion7(); // confirmed: GuidV7 is working!
     msg.SetProperty("CommandType", command.GetType().Name);
     return msg;
   }
 
   public static Message ToMessage(this IEvent @event)
   {
-    var msg = new Message(JsonSerializer.Serialize(@event));
+    // Use the options here!
+    var json = JsonSerializer.Serialize((object)@event, _options);
+    var msg = new Message(json);
+
     msg.MessageType = @event.GetType().FullName;
     msg.CorrelationId = Guid.CreateVersion7();
     msg.SetProperty("EventType", @event.GetType().Name);
