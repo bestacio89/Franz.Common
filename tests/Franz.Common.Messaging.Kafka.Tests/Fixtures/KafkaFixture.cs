cdf6fd3ae2676850c29ce01ae31611eaf84ef038
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-03-20 11:59:30 +0100

    [Feat]: Add new Kafka messaging unit/integration test project

---
+++ tests/Franz.Common.Messaging.Kafka.Tests/Fixtures/KafkaFixture.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+using Testcontainers.Kafka;
+using Xunit;
+
+namespace Franz.Common.Messaging.Kafka.Tests.Fixtures;
+
+public sealed class KafkaContainerFixture : IAsyncLifetime
+{
+  private readonly KafkaContainer _container;
+
+  public string BootstrapServers => _container.GetBootstrapAddress();
+
+  public KafkaContainerFixture()
+  {
+    // Use the constructor that takes the Docker image explicitly
+    _container = new KafkaBuilder("confluentinc/cp-kafka:7.6.1")
+        .WithCleanUp(true)
+        .Build();
+  }
+
+  public async Task InitializeAsync() => await _container.StartAsync();
+
+  public async Task DisposeAsync() => await _container.DisposeAsync();
+}
+
+[CollectionDefinition("Kafka")]
+public class KafkaCollection : ICollectionFixture<KafkaContainerFixture> { }
