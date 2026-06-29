c631b5323d26a519e59c969f96b41789764c6056
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-29 12:52:38 +0200

    [Fix][Persistence]: implement real ef transactions in transactionpipeline

--- sources/Franz.Common.EntityFramework/EFUnitOfWork.cs
+++ sources/Franz.Common.EntityFramework/EFUnitOfWork.cs
@@ -1,18 +1,48 @@
 ﻿using Franz.Common.EntityFramework;
+using Franz.Common.Mediator.Pipelines.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Franz.Common.EntityFramework;
 
-public class EfUnitOfWork<TDbContext> : IUnitOfWork
+public class EfUnitOfWork<TDbContext> :
+    IUnitOfWork
     where TDbContext : DbContext
 {
   private readonly TDbContext _db;
+  private IDbContextTransaction? _transaction;
 
   public EfUnitOfWork(TDbContext db)
   {
     _db = db;
   }
 
+  // Franz.Common.EntityFramework.IUnitOfWork
   public Task<int> CommitAsync(CancellationToken ct = default)
       => _db.SaveChangesAsync(ct);
+
+  // Franz.Common.Mediator.Pipelines.Core.IUnitOfWork
+  public async Task BeginAsync(CancellationToken ct = default)
+      => _transaction = await _db.Database.BeginTransactionAsync(ct);
+
+  async Task IUnitOfWork.CommitAsync(CancellationToken ct)
+  {
+    await _db.SaveChangesAsync(ct);
+    if (_transaction is not null)
+    {
+      await _transaction.CommitAsync(ct);
+      await _transaction.DisposeAsync();
+      _transaction = null;
+    }
+  }
+
+  public async Task RollbackAsync(CancellationToken ct = default)
+  {
+    if (_transaction is not null)
+    {
+      await _transaction.RollbackAsync(ct);
+      await _transaction.DisposeAsync();
+      _transaction = null;
+    }
+  }
 }
