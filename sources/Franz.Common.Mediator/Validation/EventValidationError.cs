e4870a770fdd8eb2e72d9bb8271c3006045af01f
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-02 01:53:23 +0200

    [Feat][Messaging]: introduce canonical event validation exception

---
+++ sources/Franz.Common.Mediator/Validation/EventValidationError.cs
@@ -0,0 +1,26 @@
+﻿using Franz.Common.Mediator.Pipelines.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Franz.Common.Mediator.Validation;
+
+public sealed class EventValidationException : Exception
+{
+  public ValidationResult Result { get; }
+
+  public string Code => "event.validation.failed";
+
+  public EventValidationException(ValidationResult result)
+      : base("Event validation failed")
+  {
+    Result = result ?? throw new ArgumentNullException(nameof(result));
+
+    if (!result.Errors.Any())
+      throw new ArgumentException("EventValidationException requires at least one validation error.");
+  }
+
+  public override string ToString()
+      => $"{Code}: {Message}";
+}
